04b89f75331e7b34a19757ce72d0b982e4a9204f
Amilcar Teixeira <milkadesign@gmail.com>
2024-07-17 02:56:09 -0300

    Chore(path)

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ for (const weight of ["regular", "fill"]) {
   var link = document.createElement("link");
   link.rel = "stylesheet";
   link.type = "text/css";
-  link.href =
-    "https://unpkg.com/@animaliads/animalia-icon/src/" + weight + "/style.css";
+  link.href = "https://unpkg.com/animalia-icon/src/" + weight + "/style.css";
   head.appendChild(link);
 }
